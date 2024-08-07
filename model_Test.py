71df67ac475f43e6b100714dc11263304a4b46ca
Zeytunzeynep <128320712+Zeytunzeynep@users.noreply.github.com>
2024-08-07 21:19:47 +0300

    Update model_Test.py

--- model_Test.py
+++ model_Test.py
@@ -5,27 +5,23 @@ from transformers import TFBertModel, BertTokenizer
 from sklearn.preprocessing import LabelEncoder
 import random
 
-# Modeli yüklerken custom_objects parametresini kullanarak TFBertModel'i tanıtın
+
 model_save_path_h5 = 'bert_cnn_model.h5'
 model = tf.keras.models.load_model(model_save_path_h5, custom_objects={'TFBertModel': TFBertModel})
 
-# Veri setini yükleyin
+# Veri seti
 data = pd.read_csv('model_egitim_veri.csv')
 
-# Rastgele 50 örnek seçin
 k = 100
 random_indices = random.sample(range(len(data)), k)
 sample_data = data.iloc[random_indices]
 
-# Metin ve etiket sütunlarını seçin
 title_texts = sample_data['temizlenmis_title'].tolist()
 description_texts = sample_data['temizlenmis_description'].tolist()
 category_labels = sample_data['Real/Fake'].tolist()
 
-# Tokenizer'ı yükleyin
 tokenizer = BertTokenizer.from_pretrained('dbmdz/bert-base-turkish-cased')
 
-# Tokenizasyon ve encoding işlemleri
 max_len = 256
 
 
@@ -41,27 +37,23 @@ def encode_text(texts):
     return encodings
 
 
-# Her sütunu tokenize edin
 title_encodings = encode_text(title_texts)
 description_encodings = encode_text(description_texts)
 
-# TensorFlow tensor'ları oluşturun
 input_ids = np.concatenate([title_encodings['input_ids'], description_encodings['input_ids']], axis=1)
 attention_masks = np.concatenate([title_encodings['attention_mask'], description_encodings['attention_mask']], axis=1)
 
-# Kategorik etiketleri sayısal değerlere dönüştürün
 label_encoder = LabelEncoder()
 category_labels_encoded = label_encoder.fit_transform(category_labels)
 
-# Test verisi için DataLoader oluşturun
 test_dataset = tf.data.Dataset.from_tensor_slices(
     ({'input_ids': input_ids, 'attention_mask': attention_masks}, category_labels_encoded)).batch(16)
 
-# Modeli test edin
+# Model test 
 test_predictions = model.predict(test_dataset)
 test_predictions = np.argmax(test_predictions, axis=1)
 
-# Sonuçları ve verileri dosyaya yazdırın
+
 results_file = 'haber_test_results_1.txt'
 with open(results_file, 'w', encoding='utf-8') as f:
     correct_count = 0
